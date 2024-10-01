a25680f58f6089c489217944df49ac46f9af1e31
bulla-01 <133217733+bulla-01@users.noreply.github.com>
2024-10-01 06:38:35 +0200

    My first deployment

---
+++ constant.py
@@ -0,0 +1,32 @@
+info = {
+   "Pronoun": "MR.", 
+   "Name": "Frank",
+   "Full_Name": "Frank Sarfo",
+   "Intro": "An Application Support Engineer & Data Scientist",
+   "About":"Highly skilled Data Analyst and Application Support Engineer with 6 years.' Trustworthy with Six 
+years of practical experience and dedicated work ethic. Self-motivated 
+to consistently provide first-class results in line with stringent targets 
+and deadlines.: https://cognitiveclass.ai/",
+   "Tableau":"https://public.tableau.com/app/profile/vickytc/viz/SunnybrookTeam/Overview",
+   "Medium":"https://medium.com/@vicky-note/about",
+   "City":"Toronto, Canada",
+   "Photo":"""<a href=\"https://www.linkedin.com/in/frank-sarfo-13ab96253?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BCxo6OPrmQ2iwKnH1I5iDsw%3D%3D"><img src=\"https://sn-assets.s3.us.cloud-object-storage.appdomain.cloud/projects/profile.png" width=\"200\"   alt=\"Profile\" title=\"Profile\"></a>""",
+   "Email": "francben@gmail@gmail.com"
+}
+
+embed_rss= {
+    'rss':"""<div style="overflow-y: scroll; height:500px; background-color:white;"> <div id="retainable-rss-embed" 
+        data-rss="https://medium.com/feed/@vicky-note"
+        data-maxcols="3" 
+        data-layout="grid"
+        data-poststyle="inline" 
+        data-readmore="Read the rest" 
+        data-buttonclass="btn btn-primary" 
+        data-offset="0"></div></div> <script src="https://www.twilik.com/assets/retainable/rss-embed/retainable-rss-embed.js"></script>"""
+}
+
+endorsements = {
+    "img1": "https://user-images.githubusercontent.com/90204593/238169843-12872392-f2f1-40a6-a353-c06a2fa602c5.png",
+    "img2": "https://user-images.githubusercontent.com/90204593/238171251-5f4c5597-84d4-4b4b-803c-afe74e739070.png",
+    "img3": "https://user-images.githubusercontent.com/90204593/238171242-53f7ceb3-1a71-4726-a7f5-67721419fef8.png"
+}
